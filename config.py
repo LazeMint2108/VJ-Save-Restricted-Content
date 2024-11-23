ff7fd21da11e6fb4a46d9ebd4df53a10633101ca
LazeMint2108 <hs20062108@gmail.com>
2024-11-23 10:05:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "19302621"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "be663997dc5845b6cba859db10386cce")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://LazeMint:<db_password>@cluster0.vsfvv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
